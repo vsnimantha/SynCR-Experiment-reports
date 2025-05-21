f86a865b9b6d7b823caa3c79e5a34b620ea69dd2
Nimantha <vsnkariyakarawana@gmail.com>
2025-05-21 13:35:30 +0200

    Experiment 8

---
+++ c++_reports/new_1_2025-05-13_09-19-49-448/Generated_Programs/generated_program_2025-05-13_09-19-50-277.cpp
@@ -0,0 +1,43 @@
+// Template_13.tmpl
+// Triggers ICE
+#include <iostream>
+#include <string>
+
+using namespace std;
+typedef void (*FuncPtr)(void);
+
+int main() {
+  FuncPtr
+      funcs[1000000]; // This may cause an internal compiler error due to size
+
+  float deltaBeta = 14.0f;
+
+  if (true) {
+
+    for (int i = 0; i >= 0; i++) {
+      std::cout << "Code execution started" << std::endl;
+    }
+  }
+
+  else {
+    std::cout << "0ZW5UHQBED44N154YF5G" << std::endl;
+  }
+
+  double omicron = 2 * 8;
+
+  while (false) {
+    int tau = 19 - 4;
+  }
+
+  std::cout << "BIDYNU6KD93VP06TQTLO" << std::endl;
+
+  std::cout << "This is a sample print statement" << std::endl;
+
+  std::cout << "T589OEOALCAKKWJL1EO7" << std::endl;
+
+  std::cout << "F3BUKMCRFBXN94RINSXZ" << std::endl;
+
+  string chiTheta = "EtherealGlow";
+
+  return 0;
+}

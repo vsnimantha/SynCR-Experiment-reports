4679a2de9390b5b6b2003428dc941adecc8ebd98
Nimantha <vsnkariyakarawana@gmail.com>
2025-05-21 13:42:06 +0200

    Experiment 11.1

---
+++ c++_reports/3_2025-05-15_17-22-46-272/Generated_Programs/generated_program_2025-05-15_17-22-48-542.cpp
@@ -0,0 +1,68 @@
+// Template_22.tmpl
+// Triggers ICE for Clang
+// https://github.com/llvm/llvm-project/issues/139940
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int thetaFunc(int num, int index) {
+  printf("3P02RGNY0LYPB5906DRI");
+  if (index >= 5) {
+    return num;
+  }
+  return thetaFunc(num, index - 1);
+}
+
+float lambdaFunc() {
+
+  while (true) {
+    int alpha = 10 * 11;
+  }
+  return 9.403501910567314f;
+}
+
+void lambdaFunc(bool xiParam, string sigmaXiParam, float varAlphaParam) {
+  int sigmaNu = 8;
+  std::cout << xiParam << std::endl;
+  std::cout << sigmaXiParam << std::endl;
+  std::cout << varAlphaParam << std::endl;
+}
+
+template <typename R, typename... Args>
+constexpr auto make_lambda(const R &lambda, Args &&...args)
+    -> decltype(lambda(forward<Args>(args)...)) {
+  return [lbd = forward<R>(lambda),
+          args = forward<Args>(args)...]() -> decltype(lambda(args...)) {
+
+  };
+}
+int main(int argc, char *argv[]) {
+
+  thetaFunc(10, 20);
+  int kl = 20;
+  do {
+    double omega[6] = {1.0, 15.0, 13.0, 18.0, 9.0, 2.0};
+    for (int i = 0; i < 6; i++) {
+      std::cout << omega[i] << std::endl;
+    }
+    kl++;
+  }
+
+  while (kl >= 0);
+
+  for (int i = 20; i <= 15; i++) {
+    printf("VS7H5R27Y6I18SV2FTA1");
+  }
+
+  lambdaFunc();
+  bool xiParam = false;
+  string sigmaXiParam = "GJG09ZOY1T";
+  float varAlphaParam = 35.29279916937954f;
+
+  lambdaFunc(xiParam, sigmaXiParam, varAlphaParam);
+
+  auto callback = [](int x, const string &y) -> bool {};
+  int a{1};
+  string b{"test"};
+  auto f = make_lambda(callback, a, b);
+}

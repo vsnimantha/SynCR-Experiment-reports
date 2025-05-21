2bb8792814dc449160822773428a8fa44d2c4e2d
Nimantha <vsnkariyakarawana@gmail.com>
2025-05-21 13:24:20 +0200

    Experiment 3

---
+++ c++_reports/2025-05-12_15-51-55-584/Generated_Programs/generated_program_2025-05-12_15-51-56-580.cpp
@@ -0,0 +1,63 @@
+// Template_15.tmpl
+// Triggers ICE
+// https://gcc.gnu.org/bugzilla/show_bug.cgi?id=120129
+#include <cstdio>
+#include <iostream>
+
+// Placeholders
+float thetaFunc() {
+  printf("MQ6LNTHJUYTRHJGX15NX");
+  return 0.0f;
+}
+
+double betaFunc(double lambdaParam, bool alphaUpsilonParam,
+                float thetaPrimeParam) {
+  int beta = 19 - 14;
+  std::cout << alphaUpsilonParam << std::endl;
+  std::cout << lambdaParam * thetaPrimeParam << std::endl;
+  return 19.306220391195726;
+}
+
+int main(int, char **) {
+  // Native Codes
+  decltype(auto) m = {{"john", "doe"}};
+
+  // Placeholders
+  thetaFunc();
+  double lambdaParam = 27.096352686915747;
+  bool alphaUpsilonParam = true;
+  float thetaPrimeParam = 87.17806895794337f;
+
+  betaFunc(lambdaParam, alphaUpsilonParam, thetaPrimeParam);
+  string alphaOmega = "MysticHarmony";
+
+  if (true) {
+    double phiDelta = 13 / 2;
+  }
+
+  else {
+    string iotaChi[20] = {
+        "MysticHarmony",   "CrystalCascade",   "GoldenPhoenix",
+        "RadiantEcho",     "CrystalCascade",   "CrystalCascade",
+        "MysticHarmony",   "MysticHarmony",    "MysticHarmony",
+        "EtherealGlow",    "CelestialJourney", "CelestialJourney",
+        "CrystalCascade",  "ZNZ4ZMTDIB",       "MajesticWhisper",
+        "MajesticWhisper", "GoldenPhoenix",    "CrystalCascade",
+        "MysticHarmony",   "CrystalCascade"};
+    for (int i = 0; i < 20; i++) {
+      std::cout << iotaChi[i] << std::endl;
+    }
+  }
+
+  double lambdaSigma = 5 - 19;
+
+  while (false) {
+    double omega = 17 * 6;
+  }
+
+  std::cout << "Random value calculated" << std::endl;
+
+  std::cout << "This is a sample print statement" << std::endl;
+
+  return 0;
+}

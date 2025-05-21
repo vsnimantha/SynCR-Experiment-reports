08f64873405c9b725e2a40a91d5997a351971006
Nimantha <vsnkariyakarawana@gmail.com>
2025-05-21 14:26:47 +0200

    Experiment 33

---
+++ c_reports/new_7_2025-05-15_15-21-22-854/Generated_Programs/generated_program_2025-05-15_15-21-24-257.c
@@ -0,0 +1,88 @@
+/* Template_11.tmpl */
+/* TRIGGER ICES */
+struct Recursive {
+  struct Recursive *next;
+  int data[1000];
+};
+
+void gimelFunc() {
+
+  while (false) {
+    printf("TQP9X9U7800R6WPPEI0F");
+  }
+}
+
+float heFunc(float sigmaXiParam) {
+  int piEta = 15;
+  printf(sigmaXiParam);
+  return 6.157394888512169f;
+}
+
+int main() {
+  struct Recursive r; /* This may cause an internal compiler error due to
+                         self-referential struct*/
+
+  char eta[] = "AzureFlame";
+
+  if (true) {
+    char phiDelta[10] = {'v', 't', 'K', 'o', 'u', 'X', 'j', 'p', 'v', 'z'};
+    for (int i = 0; i < 10; i++) {
+      printf("%c", phiDelta[i]);
+    }
+  }
+
+  else {
+    float xiBeta[6] = {6.0f, 7.0f, 3.0f, 20.0f, 19.0f, 11.0f};
+    for (int i = 0; i < 6; i++) {
+      printf("%f", xiBeta[i]);
+    }
+  }
+
+  double thetaPrime[17] = {7.0,  3.0, 15.0, 1.0, 16.0, 12.0, 3.0,  7.0, 8.0,
+                           12.0, 9.0, 2.0,  8.0, 20.0, 10.0, 16.0, 9.0};
+  for (int i = 0; i < 17; i++) {
+    printf("%lf", thetaPrime[i]);
+  }
+
+  int kl = 20;
+  do {
+    char betaTau[4] = {'r', 'X', 'H', 'p'};
+    for (int i = 0; i < 4; i++) {
+      printf("%c", betaTau[i]);
+    }
+    kl++;
+  }
+
+  while (kl > 20);
+
+  for (int i = 0; i >= 0; i++) {
+    int betaTau = 10 * 11;
+  }
+
+  float sigmaXiParam = 76.53092001218586f;
+
+  heFunc(sigmaXiParam);
+  gimelFunc();
+
+  if (true) {
+    char phiDelta[10] = {'v', 't', 'K', 'o', 'u', 'X', 'j', 'p', 'v', 'z'};
+    for (int i = 0; i < 10; i++) {
+      printf("%c", phiDelta[i]);
+    }
+  }
+
+  else {
+    float xiBeta[6] = {6.0f, 7.0f, 3.0f, 20.0f, 19.0f, 11.0f};
+    for (int i = 0; i < 6; i++) {
+      printf("%f", xiBeta[i]);
+    }
+  }
+
+  double iotaPrime = 18 - 1;
+
+  while (false) {
+    printf("This is a sample print statement");
+  }
+
+  return 0;
+}

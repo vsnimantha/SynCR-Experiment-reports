bf87b978396d5e2e3294db726f688e16708443e6
Nimantha <vsnkariyakarawana@gmail.com>
2025-05-21 14:24:53 +0200

    Experiment 32

---
+++ c_reports/new_6_2025-05-15_15-14-55-788/Generated_Programs/generated_program_2025-05-15_15-14-56-670.c
@@ -0,0 +1,53 @@
+/* Template_12.tmpl */
+struct LargeStruct {
+  int data[1000];
+};
+
+int main() {
+  struct LargeStruct
+      arr[100000]; /* This may cause an internal compiler error due to size */
+
+  printf("This is a sample print statement");
+
+  char tauPrime[] = "AzureFlame";
+
+  if (false) {
+    int kl = 10;
+    do {
+      double deltaBeta = 3 * 6;
+      kl++;
+    }
+
+    while (kl >= 5);
+  }
+
+  else {
+
+    while (false) {
+      double eta[18] = {15.0, 14.0, 18.0, 7.0, 13.0, 6.0,  15.0, 20.0, 2.0,
+                        14.0, 16.0, 9.0,  6.0, 20.0, 17.0, 12.0, 13.0, 4.0};
+      for (int i = 0; i < 18; i++) {
+        printf("%lf", eta[i]);
+      }
+    }
+  }
+
+  int tauTheta[2] = {19, 13};
+  for (int i = 0; i < 2; i++) {
+    printf("%d\n", tauTheta[i]);
+  }
+
+  int kl = 15;
+  do {
+    printf("Exiting function");
+    kl++;
+  }
+
+  while (kl > 10);
+
+  for (int i = 20; i > 15; i++) {
+    printf("D4T2DLG0DWYN9TJGQ7LR");
+  }
+
+  return 0;
+}

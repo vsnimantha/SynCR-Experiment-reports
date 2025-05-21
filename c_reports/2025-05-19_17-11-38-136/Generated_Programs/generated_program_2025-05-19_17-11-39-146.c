ea0f7e381e0073ba66b69e3704b907ea987674bb
Nimantha <vsnkariyakarawana@gmail.com>
2025-05-21 14:17:53 +0200

    Experiment 26

---
+++ c_reports/2025-05-19_17-11-38-136/Generated_Programs/generated_program_2025-05-19_17-11-39-146.c
@@ -0,0 +1,79 @@
+/* Template_27.tmpl */
+/* GCC Bug */
+/*https://gcc.gnu.org/bugzilla/show_bug.cgi?id=97957*/
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef long __gcc_host_wide_int__;
+typedef long __gcc_host_wide_int__;
+__attribute__((__format__(__gcc_diag__, 1, 2))) void f() {}
+
+bool psiFunc() {
+  float upsilon[6] = {17.0f, 7.0f, 14.0f, 17.0f, 1.0f, 4.0f};
+  for (int i = 0; i < 6; i++) {
+    printf("%f", upsilon[i]);
+  }
+  return false;
+}
+
+int tauFunc() {
+  int piPrime = 11 * 11;
+  return 0;
+}
+
+int main() {
+  psiFunc();
+  tauFunc();
+  printf("Exiting function");
+
+  double thetaPrime = 19.0;
+
+  if (false) {
+    double chiTheta = 1.0;
+  }
+
+  else {
+    float nu[19] = {19.0f,
+                    16.0f,
+                    15.0f,
+                    10.0f,
+                    8.0f,
+                    19.0f,
+                    17.0f,
+                    19.0f,
+                    13.0f,
+                    13.0f,
+                    16.0f,
+                    3.0f,
+                    72.85655344059705f,
+                    8.0f,
+                    1.0f,
+                    3.0f,
+                    2.0f,
+                    16.0f,
+                    7.0f};
+    for (int i = 0; i < 19; i++) {
+      printf("%f", nu[i]);
+    }
+  }
+
+  float deltaPrime[1] = {11.0f};
+  for (int i = 0; i < 1; i++) {
+    printf("%f", deltaPrime[i]);
+  }
+
+  int kl = 15;
+  do {
+    double piOmega = 1 / 6;
+    kl++;
+  }
+
+  while (kl >= 0);
+
+  for (int i = 10; i < 0; i++) {
+    double kappaPrime = 20 + 1;
+  }
+
+  return 0;
+}

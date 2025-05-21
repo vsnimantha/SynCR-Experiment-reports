1bffadbc9859fd3459bc40716968829da08b604d
Nimantha <vsnkariyakarawana@gmail.com>
2025-05-21 14:16:59 +0200

    Experiment 25

---
+++ c_reports/2025-05-19_16-58-47-258/Generated_Programs/generated_program_2025-05-19_16-58-48-398.c
@@ -0,0 +1,73 @@
+/* Template_26.tmpl */
+/* GCC Bug */
+/*https://gcc.gnu.org/bugzilla/show_bug.cgi?id=93160*/
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+extern long unsigned int sub_0(const char *);
+
+extern void *sub_1(long unsigned int);
+
+extern int var_0;
+
+void *var_1 = &var_0;
+
+register int var_0 asm("%ecx");
+
+int xiFunc() {
+  double nuChi = 2 + 13;
+  return 81;
+}
+
+int iotaFunc() {
+  int kl = 10;
+  do {
+    int deltaMu = 6 / 5;
+    kl++;
+  }
+
+  while (kl < 0);
+  return 24;
+}
+
+int main() {
+  xiFunc();
+  iotaFunc();
+  printf("This is a print statement");
+
+  double sigmaAlpha = 6.0;
+
+  if (true) {
+    int rhoPrime = 17 / 17;
+  }
+
+  else {
+
+    while (false) {
+      printf("This is a sample print statement");
+    }
+  }
+
+  float muUpsilon[6] = {20.0f, 15.0f, 14.0f, 16.0f, 19.0f, 15.0f};
+  for (int i = 0; i < 6; i++) {
+    printf("%f", muUpsilon[i]);
+  }
+
+  int kl = 20;
+  do {
+    int psi[11] = {10, 11, 19, 17, 7, 14, 14, 50, 5, 5, 3};
+    for (int i = 0; i < 11; i++) {
+      printf("%d\n", psi[i]);
+    }
+    kl++;
+  }
+
+  while (kl < 5);
+
+  for (int i = 5; i < 5; i++) {
+    printf("M5WP0C9RE8YVW3AJUS7S");
+  }
+
+  return 0;
+}

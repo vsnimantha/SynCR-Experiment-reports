bfcf4c48d37ed930b3fb4b24ebe6d49baf0162e5
Nimantha <vsnkariyakarawana@gmail.com>
2025-05-21 13:59:18 +0200

    Experiment 18

---
+++ c_reports/2025-05-19_09-28-46-907/Generated_Programs/generated_program_2025-05-19_09-28-47-971.c
@@ -0,0 +1,70 @@
+/* Template_19.tmpl */
+/* GCC Bug */
+/*https://gcc.gnu.org/bugzilla/show_bug.cgi?id=97991*/
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+#define foo _Pragma("pack(bar)")
+#pragma redefine_extname foo
+
+bool alphaFunc() {
+  double nuGamma[5] = {12.0, 19.0, 89.05723932554899, 13.0, 20.0};
+  for (int i = 0; i < 5; i++) {
+    printf("%lf", nuGamma[i]);
+  }
+  return true;
+}
+
+float iotaFunc(char alphaUpsilonParam, string rhoOmegaParam,
+               string kappaParam) {
+  char sigmaPsi = 'f';
+  printf(rhoOmegaParam);
+  printf(kappaParam);
+  return 0.0f;
+}
+
+int main() {
+  alphaFunc();
+  char alphaUpsilonParam = 'N';
+  string rhoOmegaParam = "5MJBCSQV79";
+  string kappaParam = "QR5B4L91AE";
+
+  iotaFunc(alphaUpsilonParam, rhoOmegaParam, kappaParam);
+  printf("M1HIM2MZ98EQCHAWWGVN");
+
+  float piOmega = 20.0f;
+
+  if (true) {
+    printf("This is a sample print statement");
+  }
+
+  else {
+    int piDelta = 4 + 1;
+  }
+
+  char lambdaPrime[6] = {'d', 'u', 'C', 'z', 'h', 'R'};
+  for (int i = 0; i < 6; i++) {
+    printf("%c", lambdaPrime[i]);
+  }
+
+  int kl = 15;
+  do {
+    char etaPrime[72] = {};
+    for (int i = 0; i < 0; i++) {
+      printf("%c", etaPrime[i]);
+    }
+    kl++;
+  }
+
+  while (kl > 15);
+
+  for (int i = 20; i < 0; i++) {
+    char omegaPrime[4] = {'X', 'L', 'Q', 'L'};
+    for (int i = 0; i < 4; i++) {
+      printf("%c", omegaPrime[i]);
+    }
+  }
+
+  return 0;
+}

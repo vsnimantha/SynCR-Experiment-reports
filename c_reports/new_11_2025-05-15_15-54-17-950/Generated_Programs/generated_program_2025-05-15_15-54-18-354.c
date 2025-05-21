bb42352d93574c4f30018dd3abe9febfd2934e90
Nimantha <vsnkariyakarawana@gmail.com>
2025-05-21 14:30:15 +0200

    Experiment 37

---
+++ c_reports/new_11_2025-05-15_15-54-17-950/Generated_Programs/generated_program_2025-05-15_15-54-18-354.c
@@ -0,0 +1,36 @@
+/* Template_7.tmpl */
+/* TRIGGERS ICES*/
+#include <limits.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+double reshFunc() {
+  char thetaIota = 's';
+  return 94.3841126903204;
+}
+
+double gammaPrimeFunc(char deltaPsiParam) {
+  printf("MIXZWF8U8TUGD8DO4RPU");
+  return 71.51775567551245;
+}
+
+int main() {
+
+  if (false) {
+    printf("This is a sample print statement");
+  }
+
+  else {
+    double theta = 5 / 11;
+  }
+
+  char deltaPsiParam = 'Y';
+
+  gammaPrimeFunc(deltaPsiParam);
+  reshFunc();
+
+  int large_array[1000000000]; /* This may cause an internal compiler error due
+                                  to memory limits TRIGGERS ICE */
+
+  return 0;
+}

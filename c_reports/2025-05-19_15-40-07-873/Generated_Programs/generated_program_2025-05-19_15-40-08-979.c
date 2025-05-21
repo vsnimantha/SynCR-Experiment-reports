1122f7d0e62eba713daebe29656dc4e6162615c7
Nimantha <vsnkariyakarawana@gmail.com>
2025-05-21 14:08:09 +0200

    Experiment 22

---
+++ c_reports/2025-05-19_15-40-07-873/Generated_Programs/generated_program_2025-05-19_15-40-08-979.c
@@ -0,0 +1,84 @@
+/* Template_22.tmpl */
+/* GCC Bug*/
+/*https://gcc.gnu.org/bugzilla/show_bug.cgi?id=117245*/
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+void a() {
+  int b;
+  struct {
+    char c[b];
+  } bar() {
+  }
+  struct bar {
+    __attribute__((vector_size(4))) char c[b];
+  } (*d)();
+  struct bar e() { struct bar f; }
+  d = e;
+  sizeof(d());
+}
+
+float nuPrimeFunc() {
+
+  while (true) {
+    double psiMu[6] = {5.0, 20.0, 13.0, 15.0, 18.0, 18.0};
+    for (int i = 0; i < 6; i++) {
+      printf("%lf", psiMu[i]);
+    }
+  }
+  return 0.0f;
+}
+
+void chetFunc() {
+
+  while (false) {
+    double betaPrime = 19 - 7;
+  }
+}
+
+int main() {
+  nuPrimeFunc();
+  chetFunc();
+  printf("Hello World");
+
+  float zeta = 71.89608591826568f;
+
+  if (true) {
+    char tauTheta[18] = {'y', 'F', 'o', 'b', 'v', 'e', 'X', 'm', 'V',
+                         'u', 'v', 'l', 'a', 'Q', 'P', 'q', 'X', 'o'};
+    for (int i = 0; i < 18; i++) {
+      printf("%c", tauTheta[i]);
+    }
+  }
+
+  else {
+    float phiBeta[8] = {19.0f, 3.0f, 5.0f, 10.0f, 8.0f, 13.0f, 12.0f, 17.0f};
+    for (int i = 0; i < 8; i++) {
+      printf("%f", phiBeta[i]);
+    }
+  }
+
+  float tau[4] = {14.0f, 15.0f, 14.0f, 5.0f};
+  for (int i = 0; i < 4; i++) {
+    printf("%f", tau[i]);
+  }
+
+  int kl = 20;
+  do {
+    printf("This is a sample print statement");
+    kl++;
+  }
+
+  while (kl >= 20);
+
+  for (int i = 0; i >= 0; i++) {
+    char piOmega[20] = {'g', 'A', 'A', 'r', 'w', 'u', 't', 'M', 'n', 'z',
+                        'V', 't', 'f', 'c', 'O', 'N', 'Q', 'F', 'k', 'F'};
+    for (int i = 0; i < 20; i++) {
+      printf("%c", piOmega[i]);
+    }
+  }
+
+  return 0;
+}

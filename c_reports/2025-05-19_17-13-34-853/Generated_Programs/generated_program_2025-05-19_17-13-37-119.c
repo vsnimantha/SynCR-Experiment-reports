0638dd52789c623be2a14df5fe7b28b9b9117469
Nimantha <vsnkariyakarawana@gmail.com>
2025-05-21 14:18:53 +0200

    Experiment 27

---
+++ c_reports/2025-05-19_17-13-34-853/Generated_Programs/generated_program_2025-05-19_17-13-37-119.c
@@ -0,0 +1,64 @@
+/* Template_28.tmpl */
+/* GCC Bug */
+/*https://gcc.gnu.org/bugzilla/show_bug.cgi?id=101352*/
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+void g(i) int i __attribute__((__vector_size__(16)));
+{}
+
+double rhoPrimeFunc() {
+  int psiPrime = 17;
+  return 0.0;
+}
+
+int etaPrimeFunc() {
+  int kl = 10;
+  do {
+    printf("4WZRXX11G235TRZZT8RF");
+    kl++;
+  }
+
+  while (kl <= 10);
+  return 40;
+}
+
+int main() {
+  rhoPrimeFunc();
+  etaPrimeFunc();
+  printf("This is a sample print statement");
+
+  char upsilonPhi = 'Q';
+
+  if (false) {
+    char nuPrime = 't';
+  }
+
+  else {
+    int phiBeta = 17;
+  }
+
+  double eta[11] = {10.0, 10.0, 13.0, 19.0, 14.0, 18.0,
+                    2.0,  5.0,  1.0,  3.0,  17.0};
+  for (int i = 0; i < 11; i++) {
+    printf("%lf", eta[i]);
+  }
+
+  int kl = 15;
+  do {
+    char nu[12] = {'u', 'g', 'e', 'V', 'L', 'P', 'O', 'Z', 'N', 'W', 'L', 'U'};
+    for (int i = 0; i < 12; i++) {
+      printf("%c", nu[i]);
+    }
+    kl++;
+  }
+
+  while (kl >= 5);
+
+  for (int i = 20; i <= 0; i++) {
+    printf("Status update");
+  }
+
+  return 0;
+}

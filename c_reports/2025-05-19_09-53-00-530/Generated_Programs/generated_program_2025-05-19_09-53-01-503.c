f7e4a1402a1a4fe0240fb2a1e9524c90a589a421
Nimantha <vsnkariyakarawana@gmail.com>
2025-05-21 14:05:42 +0200

    Experiment 19

---
+++ c_reports/2025-05-19_09-53-00-530/Generated_Programs/generated_program_2025-05-19_09-53-01-503.c
@@ -0,0 +1,70 @@
+/* Template_20.tmpl */
+/* GCC Bug*/
+/*https://gcc.gnu.org/bugzilla/show_bug.cgi?id=120033*/
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+#define N(q, r)
+#pragma message " " N(d, {
+})
+#pragma message " " 2 N(e, f)
+
+float etaFunc ()
+{
+  double betaNu = 7.0;
+  return 0.0f;
+}
+
+float psiFunc(float phiTauParam) {
+  char alphaPrime = 'L';
+  printf(phiTauParam);
+  return 23.421260750518137f;
+}
+
+int main() {
+  etaFunc();
+  float phiTauParam = 10.213603550652062f;
+
+  psiFunc(phiTauParam);
+  printf("S36Z8DA4HB4OWEPZNAOF");
+
+  char gammaOmicron = 'R';
+
+  if (true) {
+
+    while (false) {
+      printf("Security check passed");
+    }
+  }
+
+  else {
+
+    while (true) {
+      printf("Exiting function");
+    }
+  }
+
+  double phiPsi[5] = {5.0, 13.0, 18.0, 5.0, 3.0};
+  for (int i = 0; i < 5; i++) {
+    printf("%lf", phiPsi[i]);
+  }
+
+  int kl = 10;
+  do {
+    char epsilonPhi[18] = {'b', 'p', 'V', 'B', 'K', 'L', 'G', 'A', 'n',
+                           'B', 'u', 'P', 'P', 'x', 'd', 'F', 'p', 'L'};
+    for (int i = 0; i < 18; i++) {
+      printf("%c", epsilonPhi[i]);
+    }
+    kl++;
+  }
+
+  while (kl <= 10);
+
+  for (int i = 20; i >= 0; i++) {
+    printf("Security check passed");
+  }
+
+  return 0;
+}

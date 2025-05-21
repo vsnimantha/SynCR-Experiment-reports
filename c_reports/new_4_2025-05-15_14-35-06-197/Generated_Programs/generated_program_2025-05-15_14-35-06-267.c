ff97d30239da77b3189fcb5fa1097aa3d4ea8ea8
Nimantha <vsnkariyakarawana@gmail.com>
2025-05-21 14:22:40 +0200

    Experiment 30

---
+++ c_reports/new_4_2025-05-15_14-35-06-197/Generated_Programs/generated_program_2025-05-15_14-35-06-267.c
@@ -0,0 +1,44 @@
+/* Template_14.tmpl */
+/* TRIGGERS ICE */
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+struct Level3 {
+  int data;
+};
+struct Level2 {
+  struct Level3 level3;
+};
+struct Level1 {
+  struct Level2 level2;
+};
+struct Root {
+  struct Level1 level1;
+};
+bool tavFunc() {
+  char upsilonGamma[] = "RadiantEcho";
+  return true;
+}
+double gammaFunc() {
+  char theta[] = "CelestialJourney";
+  return 0.0;
+}
+int main() {
+  struct Root
+      r; /* This may cause an internal compiler error due to deep nesting */
+  tavFunc();
+  gammaFunc();
+  if (true) {
+    int kl = 20;
+    do {
+      printf("3OTATRH8K2JBV81YSTWG");
+      kl++;
+    }
+    while (kl <= 15);
+  }
+  else {
+    double muUpsilon = 13 * 3;
+  }
+  double etaPsi = 15 / 1;
+  return 0;
+}

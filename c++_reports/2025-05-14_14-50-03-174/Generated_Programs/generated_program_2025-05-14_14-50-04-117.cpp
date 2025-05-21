42020bbb64831907bdab4529b4b100662d8f105a
Nimantha <vsnkariyakarawana@gmail.com>
2025-05-21 13:33:58 +0200

    Experiment 7

---
+++ c++_reports/2025-05-14_14-50-03-174/Generated_Programs/generated_program_2025-05-14_14-50-04-117.cpp
@@ -0,0 +1,66 @@
+// Template_19.tmpl
+// Triggers ICE for Clang
+// https://github.com/llvm/llvm-project/issues/102106
+#include <iostream>
+
+struct A {
+  A(...);
+};
+
+int upsilonFunc(int num, int index) {
+  printf("RZU3DMI36STUWR875IV4");
+  if (index >= 5) {
+    return num;
+  }
+  return upsilonFunc(num, index - 1);
+}
+
+float alphaFunc() {
+  int kl = 10;
+  do {
+    double xi[20] = {5.0,  2.0,  10.0, 14.0, 4.0,  14.0, 18.0,
+                     2.0,  7.0,  4.0,  10.0, 13.0, 6.0,  5.0,
+                     13.0, 18.0, 17.0, 16.0, 11.0, 15.0};
+    for (int i = 0; i < 20; i++) {
+      std::cout << xi[i] << std::endl;
+    }
+    kl++;
+  }
+
+  while (kl >= 10);
+  return 31.986026787272415f;
+}
+
+double sigmaFunc(int alphaEpsilonParam) {
+
+  while (true) {
+    double iotaChi = 1 * 16;
+  }
+  std::cout << alphaEpsilonParam << std::endl;
+  return 16.04691684342171;
+}
+
+int main() {
+
+  volatile A a;
+  volatile A a2(a);
+
+  upsilonFunc(10, 20);
+  int kl = 20;
+  do {
+    int phiPsi = 10 - 7;
+    kl++;
+  }
+
+  while (kl < 20);
+
+  for (int i = 10; i < 5; i++) {
+    printf("FO1ULJMF6JUQ5EBI2YN2");
+  }
+
+  alphaFunc();
+  int alphaEpsilonParam = 24;
+
+  sigmaFunc(alphaEpsilonParam);
+  return 0;
+}
